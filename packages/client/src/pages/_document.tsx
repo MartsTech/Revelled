52c7f62101d2a85ee291fa3c518a0a9cc8cb1642
Martin Velkov <martin.e.velkov@gmail.com>
2021-12-11 18:04:40 +0200

    back changes

--- packages/client/src/pages/_document.tsx
+++ packages/client/src/pages/_document.tsx
@@ -6,10 +6,7 @@ import NextDocument, {
   NextScript,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
-
-const APP_NAME = "Revelled";
-const APP_DESCRIPTION =
-  "A public event management and advertisement platform for interactive events.";
+import { defaultSEO } from "utils/seo";
 
 class Document extends NextDocument {
   static async getInitialProps(ctx: DocumentContext) {
@@ -42,14 +39,14 @@ class Document extends NextDocument {
     return (
       <Html lang="en" dir="ltr">
         <Head>
-          <meta name="application-name" content={APP_NAME} />
+          <meta name="application-name" content={defaultSEO.title} />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
             content="default"
           />
-          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
-          <meta name="description" content={APP_DESCRIPTION} />
+          <meta name="apple-mobile-web-app-title" content={defaultSEO.title} />
+          <meta name="description" content={defaultSEO.description} />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#0b0e11" />
